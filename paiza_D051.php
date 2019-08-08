2f1b300bde5245a38709aa59dec697fc5ccbd8ff
shin304 <s.oono@asto-system.com>
2019-08-08 12:18:42 +0900

    不明点　格納後の処理の仕方

---
+++ paiza_D051.php
@@ -0,0 +1,33 @@
+<?php
+   
+
+    $line = trim(fgets(STDIN));
+    $strings = explode(" ", $line);
+    var_dump($strings);
+    
+    // 手順１．配列に格納 2.Wの値をカウント 3.if文で条件を設定して出力
+    // 不明点　格納後の処理の仕方
+    // 状態　
+//     array(10) {
+//   [0]=>
+//   string(1) "W"
+//   [1]=>
+//   string(1) "W"
+//   [2]=>
+//   string(1) "W"
+//   [3]=>
+//   string(1) "W"
+//   [4]=>
+//   string(1) "W"
+//   [5]=>
+//   string(1) "S"
+//   [6]=>
+//   string(1) "S"
+//   [7]=>
+//   string(1) "S"
+//   [8]=>
+//   string(1) "S"
+//   [9]=>
+//   string(1) "S"
+// }
+?>
