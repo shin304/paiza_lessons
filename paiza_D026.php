444f11870099615453de57a0be81a589c1cba1e9
shin304 <s.oono@asto-system.com>
2019-08-08 12:02:09 +0900

    不明点：配列に値を格納した後に、noの数をカウントする方法

---
+++ paiza_D026.php
@@ -0,0 +1,11 @@
+<?php
+    
+
+    while($input = fgets(STDIN)) {
+        $strings[] = trim($input);
+    }
+    // var_dump($strings);
+    
+    //　手順　1.格納　2.noの数をカウントして出力
+    //　分からない点　配列に値を格納した後に、noの数をカウントする方法
+?>
