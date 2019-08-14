a24bc861a380548ca9a3aeca71583e7cb2b03452
shin304 <s.oono@asto-system.com>
2019-08-14 13:30:44 +0900

    修正完了

--- paiza_D058.php
+++ paiza_D058.php
@@ -1,16 +1,53 @@
 <?php
    
 
-    $line = trim(fgets(STDIN));
-    $nums = explode(" ", $line);
-    // var_dump($nums);
+//     $line = trim(fgets(STDIN));
+//     $nums = explode(" ", $line);
+//     // var_dump($nums);
     
-    // 改善点　foreachなどを使ったより簡単なプログラムの有無
+//     // 改善点　foreachなどを使ったより簡単なプログラムの有無
     
-   $A = str_repeat("A", $nums[0]);
-   $B = str_repeat("B", $nums[1]);
+//    $A = str_repeat("A", $nums[0]);
+//    $B = str_repeat("B", $nums[1]);
    
-   echo $A . $B . $A;
+//    echo $A . $B . $A;
    
-   // テストはclear
+//    // テストはclear
+
+$input = trim(fgets(STDIN));
+$items = explode(" ", $input);
+
+
+$count = 1;
+$answer;
+
+foreach ($items as $item) {
+    if ($count == 1) {
+        for ($i = 1; $i <= $item; $i++) {
+            if($i == 1) {
+                $answer = "A";
+            } else {
+                $answer .= "A";
+            }
+        }
+        $count++;
+    } elseif ($count == 2) {
+        for($i = 1; $i <= $item; $i++) {
+            $answer .= "B";
+        }
+        $count++;
+    } else {
+        for ($i = 1; $i <= $item; $i++) {
+            $answer .= "A";
+             
+        }
+    }
+        
+}
+
+        echo $answer;
+        
+        
+        
+        
 ?>
