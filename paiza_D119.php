8a08746ad6356354c93eb8957b4efff03fc00d94
shin304 <s.oono@asto-system.com>
2019-08-15 11:24:56 +0900

    修正完了

--- paiza_D119.php
+++ paiza_D119.php
@@ -1,7 +1,14 @@
 <?php
     //  テストでは9/10正解だったのですがどうしても最後の一文が正解になりません
 
+    //　修正前
     $input = trim(fgets(STDIN));
     $pai = 3.141592653589793;
+    echo substr($pai, 0, $input + 2) . PHP_EOL;
+
+
+    //  修正後　$pai を文字列に
+    $input = trim(fgets(STDIN));
+    $pai = "3.141592653589793";
     echo substr($pai, 0, $input + 2) . PHP_EOL;
 ?>
