86ff08a4d7b71e0bba17da6baffc8f7d5e1d8632
shin304 <s.oono@asto-system.com>
2019-08-19 14:17:36 +0900

    修正完了　配列の値が重複した時の考え方が分かりました！

--- paiza_D079.php
+++ paiza_D079.php
@@ -1,33 +1,15 @@
 <?php
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+
+    $pass = trim(fgets(STDIN));
+    $passArray = str_split($pass);
+    $passNum = count (array_unique($passArray) );
+
+    if ($passNum == 1) {
+        echo "NG";
+        exit;
+    } 
     
-    //  やろうとしたこと 
-    //  文字列を配列に格納　if文を使い、それぞれの値が全て等しければNGを出力。それ以外はそのまま出力
-    
-    //  不明点
-    //  配列のすべての値が等しいかどうか確認する方法
-    
-    $line = trim(fgets(STDIN));
-    $arr = str_split($line);
-    // var_dump($arr);
-    
-    
-//     array(8) {
-//   [0]=>
-//   string(1) "A"
-//   [1]=>
-//   string(1) "A"
-//   [2]=>
-//   string(1) "A"
-//   [3]=>
-//   string(1) "A"
-//   [4]=>
-//   string(1) "A"
-//   [5]=>
-//   string(1) "A"
-//   [6]=>
-//   string(1) "A"
-//   [7]=>
-//   string(1) "A"
-// }
-    
-?>
+    echo "OK";
+  
