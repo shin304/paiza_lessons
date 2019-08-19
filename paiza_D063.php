eb42657eb9dad5c816a4a34daeb7c43f8b954771
shin304 <s.oono@asto-system.com>
2019-08-19 13:47:37 +0900

    並び順の求め方、理解できました！

--- paiza_D063.php
+++ paiza_D063.php
@@ -1,6 +1,12 @@
 <?php
-   
-    // 解き方の手順が分かりません
-    $input_line = fgets(STDIN);
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
 
+    $order = explode(" ",trim(fgets(STDIN)));
+    $self = trim(fgets(STDIN));
+    $order[] = $self;
+    // var_dump($order);die();
+    sort($order);
+    // var_dump($order);
+    echo array_search($self, $order) + 1;
 ?>
