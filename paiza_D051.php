af78b03e01d732c2a37ada9ab665264b65c1f22c
shin304 <s.oono@asto-system.com>
2019-08-19 13:00:10 +0900

    D051　修正完了

--- paiza_D051.php
+++ paiza_D051.php
@@ -1,33 +1,27 @@
 <?php
    
 
-    $line = trim(fgets(STDIN));
-    $strings = explode(" ", $line);
-    var_dump($strings);
+    $input = trim(fgets(STDIN));
+    $seasons = explode(" ", $input);
+    // var_dump($seasons);die;
     
-    // 手順１．配列に格納 2.Wの値をカウント 3.if文で条件を設定して出力
-    // 不明点　格納後の処理の仕方
-    // 状態　
-//     array(10) {
-//   [0]=>
-//   string(1) "W"
-//   [1]=>
-//   string(1) "W"
-//   [2]=>
-//   string(1) "W"
-//   [3]=>
-//   string(1) "W"
-//   [4]=>
-//   string(1) "W"
-//   [5]=>
-//   string(1) "S"
-//   [6]=>
-//   string(1) "S"
-//   [7]=>
-//   string(1) "S"
-//   [8]=>
-//   string(1) "S"
-//   [9]=>
-//   string(1) "S"
-// }
-?>
+    $s = 0;
+    $w = 0;
+    
+    foreach ($seasons as $season) {
+        
+        if ($season ==  'S') {
+           $s[] = $season;
+           $s++;
+        } elseif ($season == 'W') {
+           $w[] = $season;
+           $w++;
+          }
+    }
+    //   var_dump($w);die();
+    if ($w >= 5) {
+        echo "OK";
+    } elseif ($w <= 4) {
+        echo "NG";
+    }
+   
