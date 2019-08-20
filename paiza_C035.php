0666d5e738ae3dc2808d369ac314c96a4b4504de
shin304 <s.oono@asto-system.com>
2019-08-20 18:53:36 +0900

    mergeお願いします

--- paiza_C035.php
+++ paiza_C035.php
@@ -4,17 +4,12 @@ $num = trim(fgets(STDIN));
     while($input = fgets(STDIN)) {
         $results[] = trim($input);
     }
-    // var_dump($results); die();
-    $passMembers = 0;
     
+    $passMembers = 0;
     foreach ($results as $result) {
-        // var_dump($result); die();
         $items = explode(' ', $result);
-        // var_dump($items); die();
         $ls = array_splice($items, 0, 1)[0];
-        // var_dump($items); die();
         
-        // $coutn = 1;
         $sum = 0;
         foreach ($items as $item) {
             $sum += $item;
@@ -34,7 +29,6 @@ $num = trim(fgets(STDIN));
             case 'l':
                 if (($items[3] + $items[4]) >= 160) {
                     $passMembers++;
-                    // var_dump($passMembers);die();
                 }
                 break;
         }
