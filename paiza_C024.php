e393c821371be00cab03f8e1d93aba4852a511fc
shin304 <s.oono@asto-system.com>
2019-08-21 16:31:55 +0900

    C024  修正完了　0821

--- paiza_C024.php
+++ paiza_C024.php
@@ -6,30 +6,21 @@
     while($line = fgets(STDIN)) {
         $results[] = trim($line);
     }
-    // var_dump($results);die();
-    // 
     foreach ($results as $result) {
         $items = explode(" ", $result);
-        // var_dump($items);die();
-        
+
         // set add sub をわける
         $function = array_splice($items, 0, 1)[0];
-        // var_dump($function);die();
-        // var_dump($items);die();
-        
+
         $answer1 = 0;
         $answer2 = 0;
-        // var_dump($function);die();
         switch ($function) {
             case "SET" :
                 if ($items[0] == 1) {
                     $answer1 = $items[1];
-                    // var_dump($answer1);die();
                 } elseif ($items[0] == 2) {
                     $answer2 = $items[1];
-                    
                 }
-                // var_dump($answer2);die();
                 break;
             case "ADD" :
                 $answer2 = $answer1 + $items[0];
@@ -37,10 +28,7 @@
             case "SUB" :
                 $answer2 = $answer1 - $items[0];
                 break;
-                
         }
-        
-        
     }
     echo $answer1 . " " . $answer2;
 
