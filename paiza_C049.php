6c9882a79f5aca980d41da88ca0c5d70fc22b6bd
shin304 <s.oono@asto-system.com>
2019-08-21 16:25:24 +0900

    C049  修正完了　0821

--- paiza_C049.php
+++ paiza_C049.php
@@ -1,25 +1,17 @@
 <?php
-    // 自分の得意な言語で
-    // Let's チャレンジ！！
 
     $num = fgets(STDIN);
-    
     while ($line = fgets(STDIN)) {
-        $Fs[] = trim($line);
+        $stairs[] = trim($line);
     }
-    // var_dump($Fs);
-    
- 
-    // 1. 始めの階と1階との差を求める
-    $first = $num - 1;
-    
-    // 2. 今いる階と次の階の差を足していくーーー（ここから分からない）ーーー
-    foreach ($Fs as $F) {
-        $sum += $F ;
+    $now = 1;
+    $sum = 0;
+    // 今いる階と前の階の差を足していく
+    foreach ($stairs as $stair) {
+        
+        $sum += abs($stair - $now);
+        $now = $stair;
     }        
-    // var_dump($F);
-    
-    // 1+2
-    echo $first + $sum;
+    echo $sum;
 
 ?>
