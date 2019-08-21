9cffa2a7f6c7ca33acf4040758aaf76451d6f4a0
shin304 <s.oono@asto-system.com>
2019-08-21 13:09:29 +0900

    C034 修正完了　0821

--- paiza_C034.php
+++ paiza_C034.php
@@ -1,7 +1,34 @@
 <?php
-    // 自分の得意な言語で
-    // Let's チャレンジ！！
 
     $input = trim(fgets(STDIN));
+    $nums = explode(" ", $input);
+    $a = $nums[0];
+    $b = $nums[2];
+    $c = $nums[4];
+    $mark = $nums[1];
     
+    if ($a == "x") {
+        if ($mark == "+") {
+            $x = $c - $b;
+        } else {
+            $x = $c + $b;
+        }
+    }
+    
+    if ($b == "x") {
+        if ($mark == "+") {
+            $x = $c - $a;
+        } else {
+            $x = $a - $c;
+        }
+    }
+    
+    if ($c == "x") {
+        if ($mark == "+") {
+            $x = $a + $b;
+        } else {
+            $x = $a - $b;
+        }
+    }
+    echo $x;
 ?>
