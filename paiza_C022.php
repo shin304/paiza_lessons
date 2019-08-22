f06e7af35133309641f0cdc4ae921e3d0ec221a0
shin304 <s.oono@asto-system.com>
2019-08-22 12:39:33 +0900

    C022  修正完了　0822

--- paiza_C022.php
+++ paiza_C022.php
@@ -1,22 +1,29 @@
 <?php
     // 自分の得意な言語で
     // Let's チャレンジ！！
-
     $num = fgets(STDIN);
     while ($line = fgets(STDIN)) {
         $lines[] = trim($line);
     }
-    
+    // $start = explode(" ", $lines[0])[0];
     // var_dump($lines);
     // $items = [];
+    // $high = [];
     foreach ($lines as $line) {
         $items = explode(" ", $line);
+    
+            $start[] = $items[0];
+            $last[] = $items[1];
+            $high[] = $items[2];
+            $low[] = $items[3];
+            // var_dump($start);
+            // var_dump($low);
+            // 繰り返し格納されてしまうのを防ぎたい
         
-        //  start last high low を設定し出力
-        // 配列であればreset end max minで出力可能
-        // 配列でない場合の出力方法が不明
-        
+      
                 
     }
+    // var_dump($start);
+    echo reset($start) . " " . end($last) . " " . max($high) . " " . min($low);
     
-?>
+    
