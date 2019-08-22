194345e698d60532a9ba6e249c8d97a9b71a5701
shin304 <s.oono@asto-system.com>
2019-08-22 16:08:24 +0900

    C061 この方法だとできないでしょうか？　0822

--- paiza_C061.php
+++ paiza_C061.php
@@ -1,31 +1,36 @@
 <?php
-    // 自分の得意な言語で
-    // Let's チャレンジ！！
+
 
     $input = fgets(STDIN);
     $nums = explode(" ", trim($input));
-    // var_dump($nums);die();
     
-    // 合計値を出す
-    $sum = 0;
-    foreach ($nums as $num) {
-        $sum += $num;
-    }
-    
-    // 1の位の計算
+    // 0を表示
+    $nums[0] = sprintf('%03d', $nums[0]);
+    $nums[1] = sprintf('%03d', $nums[1]);
+    // var_dump($nums[0]);
+
+    // 1の位
     $nums1 = substr($nums[0], -1 ,1) + substr($nums[1], -1 ,1);
-    $num1 = substr($nums1, -1 ,1);
+    if ($nums1 >= 10) {
+        $result1 = $nums1 - 10;
+    } else $result1 = $nums1;
     
-    // 10の位の計算
-    $nums10 = substr($nums[0], -2 ,1) + substr($nums[1], -2 ,1);
-    $num10 = substr($nums10, -1 ,1);
+    // 10の位
+    $nums10 = substr($nums[0], -2 ,1) + substr($nums[1], -2 ,1); 
+    if ($nums10 >= 10) {
+        $result10 = $nums10 - 10;
+    } else $result10 = $nums10;
     
-    // 100の位の計算
+    // 100の位
     $nums100 = substr($nums[0], -3 ,1) + substr($nums[1], -3 ,1);
-    $num100 = substr($nums100, -1 ,1);
+    if ($nums100 >= 10) {
+        $result100 = $nums100 - 10;
+    } else $result100 = $nums100;
     
-    //  それぞれ出力
-    echo $num100 . $num10 . $num1;
-    
-    
-?>
+    // 整数値にして出力
+    echo intval($result100 . $result10 . $result1);
+
+    // 0がすべて消えてしまうのが問題
+    // 例 014　➡　14
+    // 文字列として出力する？
+ ?>
