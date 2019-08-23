350ea2555f3a0b86b03c8fcf2fad812697498ae8
shin304 <s.oono@asto-system.com>
2019-08-23 13:25:12 +0900

    C21　重いのか何故か処理が実行できません　0823

---
+++ paiza_C021.php
@@ -0,0 +1,29 @@
+<?php
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+
+    $base = explode(" ",fgets(STDIN));
+    $num = trim(fgets(STDIN));
+    while ($line = fgets(STDIN)) {
+        $items[] = trim($line);
+    }
+    var_dump($items);
+    foreach ($items as $item) {
+        
+        $pos = explode(" ",$item);
+        
+        // 条件式を満たすよう定義
+        $r1 = $base[2] ** 2;
+        $xcyc = ($pos[0] - $base[0]) ** 2 + ($pos[0] - $base[1]) ** 2;
+        $xcyc = ($pos[0] - $base[0]) ** 2 + ($pos[0] - $base[1]) ** 2;
+        $r2 = $base[3] ** 2;
+        if ( $r1 <= $xcyc && $xcyc <= $r2) {
+        
+        var_dump($base[3] ** 2);
+        echo "yes" . PHP_EOL;
+        } else {
+        echo "no" . PHP_EOL;
+        }
+    }
+    
+?>
