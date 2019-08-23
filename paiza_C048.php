9945a3eb346821a2bd95219c52fdb3a65716c652
shin304 <s.oono@asto-system.com>
2019-08-23 11:16:33 +0900

    C048　for文の中の条件指定　0823

--- paiza_C048.php
+++ paiza_C048.php
@@ -9,9 +9,10 @@
     $sum = 0;
     
     // 計算後の結果が0になったら処理を修了の指定方法が分からない　for文の中
-    for ($i = 0; $i <= 7; $i++) {
-        $result = floor($base * ($num[1] * 0.01));
+    for ($i = 0; $i <= 1; $i++) {
+        $result = floor($base * (1 - $num[1] * 0.01));
         $base = $result;
+        // var_dump($base);
         $sum += $result;
     }
     $answer = $num[0] + $sum;
