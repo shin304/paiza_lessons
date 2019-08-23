42594d75ffcd5d62118e69d79b2bb6bcb46112cf
shin304 <s.oono@asto-system.com>
2019-08-23 11:10:08 +0900

    C048　for文の中の条件指定　0823

---
+++ paiza_C048.php
@@ -0,0 +1,19 @@
+<?php
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+
+    $input = fgets(STDIN);
+    $num = explode(" ", trim($input));
+    // var_dump($num);
+    $base = $num[0];
+    $sum = 0;
+    
+    // 計算後の結果が0になったら処理を修了の指定方法が分からない　for文の中
+    for ($i = 0; $i <= 7; $i++) {
+        $result = floor($base * ($num[1] * 0.01));
+        $base = $result;
+        $sum += $result;
+    }
+    $answer = $num[0] + $sum;
+    echo $answer;
+?>
