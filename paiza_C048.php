39319905015cbec602019235181dd6f1a0e36724
shin304 <s.oono@asto-system.com>
2019-08-26 10:38:59 +0900

    MR　C048　修正完了　0826

--- paiza_C048.php
+++ paiza_C048.php
@@ -1,18 +1,13 @@
 <?php
-    // 自分の得意な言語で
-    // Let's チャレンジ！！
 
     $input = fgets(STDIN);
     $num = explode(" ", trim($input));
-    // var_dump($num);
     $base = $num[0];
     $sum = 0;
     
-    // 計算後の結果が0になったら処理を修了の指定方法が分からない　for文の中
     while (!($base == 0)) {
         $result = floor($base * (1 - $num[1] * 0.01));
         $base = $result;
-        // var_dump($base);
         $sum += $result;
     }
     $answer = $num[0] + $sum;
