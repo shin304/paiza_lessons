51eb35866083a83b2b3cec94af28af06dd5611e0
shin304 <s.oono@asto-system.com>
2019-08-26 10:35:56 +0900

    MR　C021（暴風域）　修正完了　0826

--- paiza_C021.php
+++ paiza_C021.php
@@ -1,29 +1,23 @@
 <?php
     
-
-    $input = trim(fgets(STDIN));
-    $base = explode(" ", $input);
+    $input = fgets(STDIN);
+    $base = explode(" ",trim($input));
     $num = trim(fgets(STDIN));
     while ($line = fgets(STDIN)) {
         $items[] = trim($line);
     }
-    // var_dump($items);
     foreach ($items as $item) {
         
         $pos = explode(" ",$item);
         
         // 条件式を満たすよう定義
-        $r1 = $base[2] ** 2;
-        $xcyc = ($pos[0] - $base[0]) ** 2 + ($pos[0] - $base[1]) ** 2;
-        $xcyc = ($pos[0] - $base[0]) ** 2 + ($pos[0] - $base[1]) ** 2;
+        $r1 = $base[2] ** 2; // 
+        $xcyc = (($pos[0] - $base[0]) ** 2) + (($pos[1] - $base[1]) ** 2);
         $r2 = $base[3] ** 2;
-        if ( $r1 <= $xcyc && $xcyc <= $r2) {
         
-        // var_dump($base[3] ** 2);
+        if ( $r1 <= $xcyc && $xcyc <= $r2) {
         echo "yes" . PHP_EOL;
         } else {
         echo "no" . PHP_EOL;
         }
     }
-    
-?>
