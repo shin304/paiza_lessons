02227f85dcd19b858c7adbbf638e3192a989d7fb
shin304 <s.oono@asto-system.com>
2019-08-26 12:55:04 +0900

    PR　C064　修正完了　0826

--- paiza_C064.php
+++ paiza_C064.php
@@ -1,31 +1,31 @@
 <?php
-    // 自分の得意な言語で
-    // Let's チャレンジ！！
+
+    // 品数と生徒数
     $input1 = fgets(STDIN);
-    $line = explode(" ", trim($input1)); // 品数と生徒数
-    // var_dump($line[0]);die();
+    $line = explode(" ", trim($input1)); 
     
+    // 各カロリー
     for ($i = 1; $i <= $line[0]; $i++) {
         $input2 = fgets(STDIN);
-        $calorys[] = trim($input2); // カロリー
+        $calorys[] = trim($input2); 
     }
-    // var_dump($calorys);die();
     
+    // 食べたグラム数
     while ($input3 = fgets(STDIN)) {
-        $amounts[] = explode(" ", trim($input3)); // グラム
+        $amounts[] = explode(" ", trim($input3)); 
     }     
-    // var_dump($amounts);
-    $sum = 0;
-    foreach ($amounts as  $amount) { // 生徒一人当たりの摂取カロリーを求め、出力
-        // var_dump($amount);
-        for ($i = 0; $i < $line[1]; $i++) {
+    
+    // 一人ずつ計算
+    // 一人の合計を出したらsumをリセット
+    foreach ($amounts as  $amount) { 
+        $sum = 0;
+        for ($i = 0; $i < $line[0]; $i++) {
             $items = floor($amount[$i] * $calorys[$i] / 100);
             $sum += $items;
-            
-            
         }
-        echo $sum;
-        // var_dump($sum);
-        // echo $items[$i];
+     // まず一人目を出力
+        echo $sum . PHP_EOL;
     }
-    // var_dump($items);
+    
+    
+    
