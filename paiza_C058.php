a2b940a453d4f341068e9563d2d747a52c8d54e4
shin304 <s.oono@asto-system.com>
2019-08-26 10:41:53 +0900

    MR　C058　修正完了　0826

--- paiza_C058.php
+++ paiza_C058.php
@@ -1,13 +1,8 @@
 <?php
-    // 自分の得意な言語で
-    // Let's チャレンジ！！
 
     $line = trim(fgets(STDIN));
     $box = explode(" ", $line);
-    // var_dump($box[1]);die();
-    
     $bases = str_split($box[2]);
-    
     $roll = 0;
     
     if ($box[1] == $box[2]) {
@@ -19,24 +14,19 @@
         
         // 抜き出した要素
         $pull = array_shift($bases); 
-        // var_dump($pull);die();
         
         // 要素を追加
         array_push($bases, $pull); 
-        // var_dump($bases);die();
         
         // 配列を結合
         $result = implode($bases);
         
         // 処理が終わったらカウント
         $roll++;
-        // var_dump($roll);die();
         if ($box[1] == $result) {
             echo $roll;
             exit;
         }
-        
-        
     }    
     
     
