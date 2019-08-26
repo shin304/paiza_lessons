abd5efb55ee2aa345a731c777a88b801ef7a2888
shin304 <s.oono@asto-system.com>
2019-08-26 10:44:18 +0900

    MR　C022　修正完了　0826

--- paiza_C022.php
+++ paiza_C022.php
@@ -1,14 +1,11 @@
 <?php
-    // 自分の得意な言語で
-    // Let's チャレンジ！！
+
     $num = fgets(STDIN);
+    
     while ($line = fgets(STDIN)) {
         $lines[] = trim($line);
     }
-    
-    // var_dump($lines);
-    // $items = [];
-    // $high = [];
+
     foreach ($lines as $line) {
         $items = explode(" ", $line);
     
@@ -16,11 +13,6 @@
         $last[] = $items[1];
         $high[] = $items[2];
         $low[] = $items[3];
-        // var_dump($last);
-        // var_dump($low);
-        
-      
-                
     }
     echo reset($start) . " " . end($last) . " " . max($high) . " " . min($low);
     
