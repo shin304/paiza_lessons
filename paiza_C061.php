74d7a9ff5c010f582b73f3e54305d051ce4d7595
shin304 <s.oono@asto-system.com>
2019-09-10 13:06:21 +0900

    MR  C061  0910

--- paiza_C061.php
+++ paiza_C061.php
@@ -1,36 +1,69 @@
 <?php
+$items = explode(' ', trim(fgets(STDIN)));
+// 各数字を一文字ずつ配列にして、逆順にする ← 桁数を揃えるときに必要
+$item1 = array_reverse(str_split($items[0]));
+$item2 = array_reverse(str_split($items[1]));
 
+// 桁数を揃える
+if (count($item1) > count($item2)) {
+    $item2 = alignLength($item1, $item2);
+} elseif (count($item2) > count($item1)) {
+    $item1 = alignLength($item2, $item1);
+}
 
-    $input = fgets(STDIN);
-    $nums = explode(" ", trim($input));
-    
-    // 0を表示
-    $nums[0] = sprintf('%03d', $nums[0]);
-    $nums[1] = sprintf('%03d', $nums[1]);
-    // var_dump($nums[0]);
+$onesPlace;
+$tensPlace;
+$hundredsPlace;
 
-    // 1の位
-    $nums1 = substr($nums[0], -1 ,1) + substr($nums[1], -1 ,1);
-    if ($nums1 >= 10) {
-        $result1 = $nums1 - 10;
-    } else $result1 = $nums1;
-    
-    // 10の位
-    $nums10 = substr($nums[0], -2 ,1) + substr($nums[1], -2 ,1); 
-    if ($nums10 >= 10) {
-        $result10 = $nums10 - 10;
-    } else $result10 = $nums10;
-    
-    // 100の位
-    $nums100 = substr($nums[0], -3 ,1) + substr($nums[1], -3 ,1);
-    if ($nums100 >= 10) {
-        $result100 = $nums100 - 10;
-    } else $result100 = $nums100;
-    
-    // 整数値にして出力
-    echo intval($result100 . $result10 . $result1);
+// 一桁ずつ計算する
+for ($i = 0; $i <= 2; $i++) {
+    // 両方とも NULL の場合は計算しない
+    if ($item1[$i] == NULL && $item2[$i] == NULL) {
+        continue;
+    }
 
-    // 0がすべて消えてしまうのが問題
-    // 例 014　➡　14
-    // 文字列として出力する？
- ?>
+    // 一の位
+    if ($i == 0) {
+        // 合計が 10 以上の場合、十の位を無視
+        $onesPlace = calculation($item1, $item2, $i);
+        // 十の位
+    } elseif ($i == 1) {
+        $tensPlace = calculation($item1, $item2, $i);
+        // 百の位
+    } else {
+        $hundredsPlace = calculation($item1, $item2, $i);
+    }
+}
+
+if ($hundredsPlace != NULL) {
+    echo $hundredsPlace;
+}
+
+if ($tensPlace != NULL) {
+    echo $tensPlace;
+}
+
+echo $onesPlace;
+
+// 桁数を揃える
+function alignLength($largerValue, $smallValue) {
+    // 不足桁数
+    $n = count($largerValue) - count($smallValue);
+    // 不足している分だけ 0 で埋める
+    for ($i = 0; $i < count($largerValue); $i++) {
+        if ($i >= (count($largerValue) - $n)) {
+            $smallValue[$i] = 0;
+        }
+    }
+    return $smallValue;
+}
+
+// 計算
+function calculation($item1, $item2, $i) {
+    // 合計が 10 以上の場合、十の位を無視
+    if (count(str_split($item1[$i] + $item2[$i])) > 1) {
+        return str_split($item1[$i] + $item2[$i])[1];
+    }
+    return str_split($item1[$i] + $item2[$i])[0];
+}
+?>
